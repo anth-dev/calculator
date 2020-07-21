4e2a92113dbe77e311020b13b4d3bc838ed2510c
Anthony Giles <omgliekpie@gmail.com>
2020-07-21 14:37:23 -0600

    practice writing and implementing tests

--- scripts.spec.js
+++ scripts.spec.js
@@ -1,4 +1,4 @@
-const add = require('./scripts');
+const { add, subtract } = require('./scripts');
 
 describe('add', function() {
     it('adds integers', function() {
@@ -6,5 +6,17 @@ describe('add', function() {
     });
     it('works with decimals', function() {
         expect(add(2.5, 2.5)).toEqual(5);
+    });
+});
+
+describe('subtract', function() {
+    it('subtracts integers', function() {
+        expect(subtract(5, 3)).toEqual(2);
+    });
+    it('can return negative numbers', function() {
+        expect(subtract(3, 5)).toEqual(-2);
+    });
+    it('works with decimals', function() {
+        expect(subtract(5, 2.5)).toEqual(2.5);
     });
 });
