aaa3d406967bf4e9e7e4807a935258cabebdb179
Anthony Giles <omgliekpie@gmail.com>
2020-08-10 15:08:52 -0600

    finish operate function

--- scripts.js
+++ scripts.js
@@ -4,7 +4,6 @@ let display = document.querySelector('.display');
 let a;
 let b;
 let operator;
-let answer;
 
 buttons.forEach(button => button.addEventListener('mousedown', function() {
     button.classList.add("down");
@@ -12,15 +11,11 @@ buttons.forEach(button => button.addEventListener('mousedown', function() {
         clearDisplay();
     } else if (this.classList.contains("operator") == true) {
         a = input.join('');
-        console.log('a is saved as ' + a);
         // store operator in operator variable
         operator = this.dataset.operation;
-        console.log(operator);
         clearDisplay();
     } else if (this.classList.contains("equals") == true) {
         b = input.join('');
-        console.log('b is saved as ' + b);
-        // clearDisplay();
         operate(a, b, operator);
     } else {
     input.push(this.textContent);
@@ -41,65 +36,34 @@ buttons.forEach(button => button.addEventListener('touchend', function() {
 }));
 
 function add (a, b) {
-    console.log('add function is running');
-    answer = a + b;
-    display.textContent = answer;
+    display.textContent = a + b;
 }
 
 function subtract (a, b) {
-    return a - b;
+    display.textContent = a - b;
 }
 
 function multiply (a, b) {
-    return a * b;
+    display.textContent = a * b;
 }
 
 function divide (a, b) {
-    return a / b;
+    display.textContent = a / b;
 }
 
-// *** The following is what happens when you leave the quotes off of your
-// switch statement choices (the code is now working) ***
-//
-// when called by using the calculator gui the operate function seems to
-// get stuck at the switch statement with 'operator function being called
-// with add' being the last item being logged
-//
-// If you call the function from the console [-example operate(2, 3, add);]
-// the calculator works and the answer is displayed on the screen but there
-// is a bit of funky output to the console. For the above example instead of
-// the expected 'operator function being called with add' the output is instead
-// 'operator function being called with function add (a, b) {
-//     console.log('add function is running');
-//     answer = a + b;
-//     display.textContent = answer;
-// }' 
-//
-// if you set the a, b, and operator variables [-example a to 2, b to 3, and
-// operator to "add"] and try to run the function 
-// [-example operate(a, b, operator);] it gets stuck at the same point
-// as with the calculator gui with 'operator function being called
-// with add' being the last item logged... this also happens if you try
-// [-example operate(2, 3, operator);] with operator being the only variable
-// passed to the function
-//
-
-
 function operate (a, b, c) {
-    console.log('operator function being called with ' + c);
     switch(c) {
         case 'add':
-            console.log('add switch case selected');
             add(+a, +b);
             break;
         case 'subtract':
-            return subtract(a, b);
+            subtract(+a, +b);
             break;
         case 'multiply':
-            return multiply(a, b);
+            multiply(+a, +b);
             break;
         case 'divide':
-            return divide(a, b);
+            divide(+a, +b);
             break;
     }
 }
